959713e11016a88b074336d8a20891663e70e21f
Alex <kuanalex96@gmail.com>
2021-05-12 19:47:37 -0400

    adding some js functionality

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const button = document.querySelector(".calc-button");
-button.addEventListener("click", (event) => {
-console.log(event);
+// const button = document.querySelector(".calc-button");
+// button.addEventListener("click", (event) => {
+//     console.log(event.target.id);
+// });
+
+// This code adds an event listener for each of the buttons on the calculator
+// Currently, clicking the button will console log the button ID
+// I will be using these button IDs to help with the calculations later
+
+const buttons = document.querySelectorAll(".calc-button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        console.log(event.target.id);
+    })
 });
