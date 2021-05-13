5ab5090bdcc7251b2d4709c6bafcc1e26ddafeee
Alex <kuanalex96@gmail.com>
2021-05-13 13:18:14 -0400

    updating js

--- script.js
+++ script.js
@@ -7,16 +7,48 @@
 // Currently, clicking the button will console log the button ID
 // I will be using these button IDs to help with the calculations later
 // Rework this function to display the value of the button when
-let displayString = [];
 
-const buttons = document.querySelectorAll(".calc-button");
-buttons.forEach((button) => {
-    button.addEventListener("click", (event) => updateDisplay(event.target.textContent, displayString));
+// Listen for key presses and determine the type of key pressed
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator-buttons')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        // do something here...
+    }
 })
 
-function updateDisplay(string, array) {
-    if (!isNaN(parseInt(string))) {
-        array.push(string);
-        document.getElementById("display").innerHTML = console.log(array.join(""));
-    }
+
+// Determine the type of key pressed using data-action attribute
+const key = e.target
+const action = key.dataset.action
+
+
+// Keys without a data-action must be a number key
+if (!action) {
+    console.log('number key')
 }
+
+// If the key has a data-action attribute that is add, subtract, multiply or divide, it is an operator
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+
+
+// const buttons = document.querySelectorAll(".calc-button");
+// buttons.forEach((button) => {
+//     button.addEventListener("click", (event) => updateDisplay(event.target.textContent, displayString));
+// })
+
+// function updateDisplay(string, array) {
+//     if (!isNaN(parseInt(string))) {
+//         array.push(string);
+//         document.getElementById("display").innerHTML = console.log(array.join(""));
+//     }
+// }
