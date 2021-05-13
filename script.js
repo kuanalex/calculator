629ac064fe9dada8765fc9360438800a1143c9d6
Alex <kuanalex96@gmail.com>
2021-05-13 14:23:35 -0400

    adding more js functionality

--- script.js
+++ script.js
@@ -9,35 +9,52 @@
 // Rework this function to display the value of the button when
 
 // Listen for key presses and determine the type of key pressed
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator-buttons')
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        // do something here...
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        // Keys without a data-action must be a number key
+        if (!action) {
+            console.log('number key');
+        }
+
+        // If the key has a data-action attribute that is add, subtract, multiply or divide, it is an operator
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!');
+        }
+
+        if (action === 'decimal') {
+            console.log('decimal key!');
+        }
+
+        if (action === 'clear') {
+            console.log('clear key!');
+        }
+
+        if (action === 'calculate') {
+            console.log('equal key!');
+        }
     }
 })
 
 
 // Determine the type of key pressed using data-action attribute
-const key = e.target
-const action = key.dataset.action
 
 
-// Keys without a data-action must be a number key
-if (!action) {
-    console.log('number key')
-}
 
-// If the key has a data-action attribute that is add, subtract, multiply or divide, it is an operator
-if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-) {
-    console.log('operator key!')
-}
+
 
 
 
