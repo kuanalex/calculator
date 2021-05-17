3c8e29465b6a6eec3a2af7a66ec944c97efd8ae1
Alex <kuanalex96@gmail.com>
2021-05-17 13:59:37 -0400

    adding keyboard support

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Select all the buttons and the screen by using 
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
 const equalsButton = document.querySelector("[data-equals]");
@@ -11,6 +12,7 @@ let secondOperand = "";
 let currentOperation = null;
 let shouldResetScreen = false;
 
+window.addEventListener("keydown", setInput);
 equalsButton.addEventListener("click", evaluate);
 clearButton.addEventListener("click", clear);
 deleteButton.addEventListener("click", deleteNumber);
@@ -80,6 +82,23 @@ function evaluate() {
 
 function roundResult(number) {
     return Math.round(number * 1000) / 1000;
+}
+
+function setInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if (e.key === ".") appendPoint();
+    if (e.key === "=" || e.key === "Enter") evaluate();
+    if (e.key === "Backspace") deleteNumber();
+    if (e.key === "Escape") clear();
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+        setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === "/") return "÷";
+    if (keyboardOperator === "*") return "×";
+    if (keyboardOperator === "-") return "−";
+    if (keyboardOperator === "+") return "+";
 }
 
 function add(a, b) {
